76c5125c6a737254574651e8769e62e18db22b0a
MartinkaOff <110162487+MartinkaOff@users.noreply.github.com>
2022-09-29 17:46:37 +0600

    Completed the first screen

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.promo-slider').slick({
+        dots: true,
+        prevArrow: '<a class="slick-prev"></a>',
+        nextArrow: '<a class="slick-next"></a>'
+    });
+});
