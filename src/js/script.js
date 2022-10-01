b2fa788c8715d89d0ad591528b3d993c302e98db
MartinkaOff <110162487+MartinkaOff@users.noreply.github.com>
2022-10-01 14:28:54 +0600

    Completed third screen and add slider

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,21 @@ $(document).ready(function() {
         prevArrow: '<a class="slick-prev"></a>',
         nextArrow: '<a class="slick-next"></a>'
     });
+    $('.present__wrapper').slick({
+        dots: true,
+        slidesToShow: 1,
+        dotsClass: 'present__dots',
+        prevArrow: '<a class="slick-prev"></a>',
+        nextArrow: '<a class="slick-next"></a>'
+    });
+
+    $('ul.present__tabs').on('click', 'li:not(.present__tab_active)', function() {
+        $(this).addClass('present__tab_active').siblings().removeClass('present__tab_active')
+    });
+    $('#slick-slide-control10').html('Prengi Production');
+    $('#slick-slide-control11').html('Prengi FMC');
+    $('#slick-slide-control12').html('Prengi Mallz Retail');
+    $('#slick-slide-control13').html('Prengi Logistic');
+    $('#slick-slide-control14').html('Prengi IT');
+    $('#slick-slide-control15').html('Prengi HR').css('border', 'none');
 });
